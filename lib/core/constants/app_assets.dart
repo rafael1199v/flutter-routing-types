0b7ff65067553288d3961b6c2354e3445a705f45
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-05-04 08:08:42 -0400

    feat: App assets class declaration with image paths

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+
+  AppAssets._();
+  
+  static const String welcome1 = "assets/images/welcome1.jpg";
+  static const String welcome2 = "assets/images/welcome2.jpg";
+  static const String welcome3 = "assets/images/welcome3.jpg";
+}
